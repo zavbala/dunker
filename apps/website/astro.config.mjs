44b82c79f1a01a84ad29de7ed03edfd12561a71d
Jeremy Zabala <jeremy.zbala@gmail.com>
2023-11-20 13:14:59 -0600

    yup

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), mdx(), react()]
+  site: "https://dunker.app",
+  integrations: [tailwind(), sitemap(), mdx(), react()],
 });
