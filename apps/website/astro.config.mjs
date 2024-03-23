aa2a7673ec6473b29fe43ad94a3f397e8bcc50d9
Jeremy Zabala <jeremy.zbala@gmail.com>
2024-03-23 08:29:13 -0600

    yup

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -2,9 +2,8 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
 import { defineConfig } from "astro/config";
-
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
