a8ca382716548590cd19f71be0eb9c1b888c2bf4
Jeremy Zabala <jeremy.zbala@gmail.com>
2024-03-23 08:26:28 -0600

    yolo

--- apps/website/README.md
+++
@@ -1,3 +0,0 @@
-Made with Astro 🚀
-
-![website](https://github.com/zavbala/dunker/assets/95885060/440137f8-6bd3-4d52-9524-9a2362e81173)
--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -4,8 +4,12 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   site: "https://dunker.app",
   integrations: [tailwind(), sitemap(), mdx(), react()],
+  adapter: vercel({ webAnalytics: { enabled: true } }),
 });
